430ee75a412768c28f380fd69ea262af3890a995
Vassia Christ <christvassia@gmail.com>
2025-09-16 17:47:16 +0300

    update config files and package.json

--- remix.config.js
+++ remix.config.js
@@ -4,8 +4,10 @@ dotenv.config();
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
+  serverBuildPath: 'build/index.js',
   serverModuleFormat: 'esm',
   future: {
-    v7_relativeSplatPath: true
+    v7_relativeSplatPath: true,
+    v2_routeConvention: true
   }
 };
