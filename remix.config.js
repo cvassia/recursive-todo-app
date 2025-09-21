0a529439100f6a4d242e63830deb2766de66cad9
Vassia Christ <christvassia@gmail.com>
2025-09-21 16:31:06 +0300

    ssr: false in remix.config to ensure static site

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,7 @@ export default {
   ignoredRouteFiles: ['**/.*'],
   serverBuildPath: 'build/index.html',
   serverModuleFormat: 'esm',
+  ssr: false,
   future: {
     v7_relativeSplatPath: true,
     // v2_routeConvention: true
